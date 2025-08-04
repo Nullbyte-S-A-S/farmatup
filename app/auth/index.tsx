550eab3d81f70a2ce412a0a8a9c23e4655e8ecce
Augusto0414 <augustopelis4@gmail.com>
2025-08-04 17:48:05 -0500

    feat: update layout and remove modal

---
+++ app/auth/index.tsx
@@ -0,0 +1,10 @@
+import { View, Text } from 'react-native'
+export default function AuthIndex() {
+    return (
+        <>
+            <View className='flex-1 bg-white px-7'>
+                <Text>Aqui el inicio de session</Text>
+            </View >
+        </>
+    )
+}
