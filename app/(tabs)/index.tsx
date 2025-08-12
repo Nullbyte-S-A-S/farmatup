76451b6c7f3a81e0341cdc9c7f69a2ed1a857b83
Augusto0414 <augustopelis4@gmail.com>
2025-08-12 16:56:17 -0500

    ✨add: DataLine component. The component is a LineChart

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,22 +1,25 @@
+import React from 'react';
 import { Stack } from 'expo-router';
-
-import { StyleSheet, View, Text } from 'react-native';
+import { StyleSheet, View } from 'react-native';
 import { ItemConten } from '~/components/ItemConten';
+import { DataLine } from '~/components/DataLine';
+
 
 export default function Home() {
+
   return (
     <>
-      <Stack.Screen options={{ title: 'Tab ' }} />
+      <Stack.Screen options={{ title: 'Tab' }} />
       <View style={styles.container}>
         <ItemConten
-          headerTitle="Mis Tareas"
+          headerTitle="Ventas diarias"
           headerButtons={[
-            { content: "Editar", onPress: () => console.log("Editar") },
+            { content: "Ver más", onPress: () => console.log("Ver más") },
           ]}
         >
-          <Text>Contenido aquí</Text>
+          <DataLine label={["Lun", "Mar", "Mié", "Jue", "Vie", "Sáb", "Dom"]}
+            data={[800, 900, 1100, 950, 1200, 1350, 1250]} />
         </ItemConten>
-
       </View>
     </>
   );
