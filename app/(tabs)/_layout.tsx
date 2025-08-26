1811f25a4b828a3fe7bd8c919b4e22758aec9ff1
DavidRaveloU <dravelo@unicesar.edu.co>
2025-08-26 15:57:17 -0500

    refactor(tabs-layout): use centralized allowedTabs from utils

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,26 +1,58 @@
+import { Ionicons } from '@expo/vector-icons';
 import { Tabs } from 'expo-router';
-import { TabBarIcon } from '../../components/TabBarIcon';
+import RoleGuard from '~/components/RoleGuard';
+import { getAllowedTabs } from '~/navegation/getAllowedTabs';
+import { adminTabs, TabOption, userTabs } from '~/navegation/tabsConfig';
+import { useAuthStore } from '~/store/auth';
+
+const uniqueByName = <T extends { name: string }>(arr: T[]) =>
+  Array.from(new Map(arr.map((t) => [t.name, t])).values());
+
+const ALL_TABS: TabOption[] = uniqueByName([...adminTabs, ...userTabs]);
 
 export default function TabLayout() {
+  const role = useAuthStore((s) => s.user?.role?.toLowerCase());
+  const allowedTabs = getAllowedTabs(role);
+  const initialRoute = allowedTabs[0].name;
   return (
-    <Tabs
-      screenOptions={{
-        tabBarActiveTintColor: 'black',
-        headerShadowVisible: false,
-      }}>
-      <Tabs.Screen
-        name="index"
-        options={{
-          title: 'Tab One',
-        }}
-      />
-      <Tabs.Screen
-        name="two"
-        options={{
-          title: 'Tab Two',
-          tabBarIcon: ({ color }) => <TabBarIcon name="code" color={color} />,
-        }}
-      />
-    </Tabs>
+    <RoleGuard>
+      <Tabs
+        initialRouteName={initialRoute}
+        screenOptions={{
+          headerShown: false,
+          tabBarStyle: {
+            paddingTop: 10,
+            alignItems: 'center',
+            position: 'absolute',
+            left: 16,
+            right: 16,
+            borderTopLeftRadius: 20,
+            borderTopRightRadius: 20,
+            backgroundColor: '#fff',
+            height: 85,
+            elevation: 5,
+            shadowOpacity: 0.1,
+          },
+          tabBarActiveTintColor: '#007BFF',
+          tabBarInactiveTintColor: '#999',
+        }}>
+        {ALL_TABS.map((tab) => {
+          const isAllowed = allowedTabs.some((t) => t.name === tab.name);
+          return (
+            <Tabs.Screen
+              key={tab.name}
+              name={tab.name}
+              options={{
+                href: isAllowed ? undefined : null,
+                title: tab.label,
+                tabBarIcon: ({ color, size }) => (
+                  <Ionicons name={tab.icon as any} size={size} color={color} />
+                ),
+              }}
+            />
+          );
+        })}
+      </Tabs>
+    </RoleGuard>
   );
 }
