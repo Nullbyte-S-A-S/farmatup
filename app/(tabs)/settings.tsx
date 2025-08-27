7af81d4da1edf37975a74eb82cdfe66a4119f68f
DavidRaveloU <dravelo@unicesar.edu.co>
2025-08-26 23:15:13 -0500

    feat(settings): enhance settings screen with profile and content sections
    - implement profile card component with image, name, role, and branch details
    - add settings content section with toggles for notifications and dark mode
    - improve ui with background color and spacing adjustments

--- app/(tabs)/settings.tsx
+++ app/(tabs)/settings.tsx
@@ -1,20 +1,40 @@
-import { SafeAreaView, ScrollView, StatusBar, StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { SafeAreaView, ScrollView, StatusBar, StyleSheet, View } from 'react-native';
+import ProfileCard from '~/components/settings/ProfileCard';
+import SettingsContentSection from '~/components/settings/SettingsContentSections';
 
 export default function SettingsScreen() {
+  const [notifications, setNotifications] = useState(false);
+  const [darkMode, setDarkMode] = useState(false);
+
   return (
     <SafeAreaView style={styles.safeArea}>
       <StatusBar barStyle="dark-content" backgroundColor="white" />
       <ScrollView
-        className="flex-1 px-7"
+        className="mt-3 flex-1 px-7"
         keyboardShouldPersistTaps="handled"
         contentContainerStyle={{ flexGrow: 1, justifyContent: 'center', alignItems: 'center' }}>
-        <View className="flex-1 items-center justify-center">
-          <Text>Ajustes</Text>
+        <View className="w-full flex-1 items-center bg-[#F3F4F6] pb-28">
+          <ProfileCard
+            className="mb-5"
+            image="https://whitenessproject.org/img/Alfredo.jpg"
+            name="Carlos Rodriguez"
+            role="Administrador"
+            branch="Sucursal Norte"
+            onEdit={() => console.log('Editar perfil')}
+          />
+          <SettingsContentSection
+            notifications={notifications}
+            setNotifications={setNotifications}
+            darkMode={darkMode}
+            setDarkMode={setDarkMode}
+          />
         </View>
       </ScrollView>
     </SafeAreaView>
   );
 }
+
 const styles = StyleSheet.create({
   safeArea: {
     flex: 1,
